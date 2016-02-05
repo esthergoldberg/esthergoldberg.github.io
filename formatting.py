03935109c49dbc6a81a4b656d304e3be9cac26f2
esthergoldberg <yoav.goldberg@gmail.com>
2016-02-05 03:54:37 +0200

    site builder

---
+++ formatting.py
@@ -0,0 +1,69 @@
+from markdown import markdown
+
+DOTS_MENU=False
+
+def do_menu(txt, title=None, fname=None):
+   res = ["<div dir='rtl' class=menu>"]
+   if DOTS_MENU:
+      res.append("""
+      <object width=100%%>  
+      <param name="movie" value="dots.swf">
+      <embed src="dots.swf" width=100%%>
+      </embed>
+      </object>
+      """)
+   if title:
+      title_lines = title.strip().split("\n")
+      res.append("<a href=index.html id=mtitle1>%s</a><a href=index.html id=mtitle2>%s</a>" % (title_lines[0],title_lines[1]))
+      
+   lines = txt.strip().split("\n")
+   last_indent = 0
+   for line in lines:
+      indent = line.count("*")
+      if indent > last_indent:
+         while (last_indent != indent):
+            res.append("<ul dir=rtl>")
+            last_indent+=1
+      if indent < last_indent:
+            res.append("</ul>")
+            last_indent-=1
+      line = line.strip().replace("*","")
+      try:
+         line,link = line.strip().split("|")
+         if link == fname:
+            lstyle="citem"
+         else: lstyle="item"
+         res.append("<li class=%s><a href=%s.html>%s</a></li>" % (lstyle,link,line))
+      except ValueError:
+         res.append("<li class=category>%s</li>" % line)
+   res.append("</div>")
+   return "\n".join(res)
+
+def do_content(txt):
+   txt = txt.replace("_S_","<b>").replace("_E_","</b>")
+   html = markdown(txt)
+   return "<div dir=rtl class=content>%s</div>" % html
+
+def do_page(html_menu, html_content, STYLE_FILE="style.css", title=""):
+   return """
+<?xml version="1.0" encoding="UTF-8"?>
+<html>
+<head>
+<META http-equiv="Content-Type" Content="text/html; charset=utf-8">
+
+<LINK REL=StyleSheet HREF="%s" TYPE="text/css" MEDIA=screen>
+</style>
+<title>%s</title>
+</head>
+<body dir=rtl>
+<object width=100%%>  
+<param name="movie" value="topBanner3.swf">
+<embed src="topBanner3.swf" width=100%%>
+</embed>
+</object>
+%s
+</body>
+</html>
+   
+   """ % (STYLE_FILE, title, " ".join([html_menu, html_content]))
+
---
+++ make.py
@@ -0,0 +1,40 @@
+from markdown import markdown 
+import codecs
+import formatting
+from collections import defaultdict
+import glob
+
+
+# read page titles
+titles = defaultdict(str)
+for line in codecs.open("pages.titles","r","utf8"):
+   line = line.strip()
+   ttl,page = line.split("|",1)
+   titles[page]=ttl
+
+menu_title        = codecs.open("menu.title","r","utf8").read()
+menu_file_content = codecs.open("menu.links","r","utf8").read()
+for fname in glob.glob("*.txt"):
+   if fname in ['groups.txt']: continue
+   print fname
+   fcontent = codecs.open(fname,"r","utf8").read()
+   fname = fname.replace(".txt","")
+
+   menu_html = formatting.do_menu(menu_file_content,menu_title,fname)
+   content_html = formatting.do_content(fcontent)
+
+   page = formatting.do_page(menu_html, content_html, "style.css", titles[fname])
+
+   fout = codecs.open(fname+".html","w","utf8")
+   fout.write(page)
+   fout.close()
+
+
+
+#menu_html = formatting.do_menu(codecs.open("menu.links","r","utf8").read(),"adhd-child")
+#content_html = formatting.do_content(codecs.open("adhd.txt","r","utf8").read())
+#html_page = formatting.do_page(menu_html, content_html)
+#html = markdown(codecs.open("adhd.txt","r","utf8").read())
+#html = markdown(codecs.open("menu.links","r","utf8").read())
+#html = markdown(codecs.open("adhd-and-learning.txt","r","cp1255").read())
+#print html_page.encode("utf8")
